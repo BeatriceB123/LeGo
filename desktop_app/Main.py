2c30049a362ae9c6007e4e5ce04f5186d70397e0
BeatriceB123 <48123183+BeatriceB123@users.noreply.github.com>
2019-12-01 22:52:44 +0200

    Create Main.py
    .

---
+++ desktop_app/Main.py
@@ -0,0 +1,8 @@
+import Interfata
+
+if __name__ == "__main__":
+    app = Interfata.QtWidgets.QApplication(Interfata.sys.argv)
+    mainWin = Interfata.MainWindow()
+    Interfata.sys.exit(app.exec_())
+
+
