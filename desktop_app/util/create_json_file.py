fb8757229acf0cc157ed7d5f2326b4ba139f2b7e
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2019-12-28 11:35:18 +0200

    qwewq

--- desktop_app/util/create_json_file.py
+++ desktop_app/util/create_json_file.py
@@ -21,11 +21,79 @@ def import_from_image():
     data['piece-list'].append(create_dict_for_json(41539, 8, 8, 1, flag=True))
     data['piece-list'].append(create_dict_for_json(63864, 1, 3, 1, flag=False))
 
+    for i in create_bridge():
+        data['piece-list'].append(i)
+
     for i in data['piece-list']:
         print(i)
 
     with open('test_export.json', 'w') as to_write:
         json.dump(data, to_write, indent=4)
+
+
+def create_bridge():
+    piece_list = []
+    piece = dict()
+    piece['id'] = 4490
+    piece['length'] = 1
+    piece['width'] = 3
+    piece['height'] = 3
+    piece['space'] = [
+        [0, 0, 0], [0, 0, 1], [0, 0, 2],
+        [0, 1, 2],
+        [0, 2, 0], [0, 2, 1], [0, 2, 2]
+    ]
+    piece['studs'] = [
+        [0, 0, 3], [0, 1, 3], [0, 2, 3]
+    ]
+    piece['tubes'] = [
+        [0, 0, 0], [0, 1, 0], [0, 2, 0]
+    ]
+    piece['image-path'] = "./lego_pictures/" + str(4490)
+    piece_list.append(piece)
+    del piece
+    piece = dict()
+    piece['id'] = 3659
+    piece['length'] = 1
+    piece['width'] = 4
+    piece['height'] = 3
+    piece['space'] = [
+        [0, 0, 0], [0, 0, 1], [0, 0, 2],
+        [0, 1, 2], [0, 2, 2],
+        [0, 3, 0], [0, 3, 1], [0, 3, 2]
+    ]
+    piece['studs'] = [
+        [0, 0, 3], [0, 1, 3], [0, 2, 3], [0, 3, 3]
+    ]
+    piece['tubes'] = [
+        [0, 0, 0], [0, 1, 0], [0, 2, 0], [0, 3, 0]
+    ]
+    piece['image-path'] = "./lego_pictures/" + str(3659)
+    piece_list.append(piece)
+    piece = dict()
+    piece['id'] = 3307
+    piece['length'] = 1
+    piece['width'] = 6
+    piece['height'] = 6
+    piece['space'] = [
+        [0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
+        [0, 1, 5], [0, 1, 4], [0, 2, 3],
+        [0, 2, 5], [0, 2, 4],
+        [0, 3, 5], [0, 3, 4],
+        [0, 4, 5], [0, 4, 4], [0, 4, 3],
+        [0, 5, 0], [0, 5, 1], [0, 5, 2], [0, 5, 3], [0, 5, 4], [0, 5, 5]
+    ]
+    piece['studs'] = [
+        [0, 0, 6], [0, 1, 6], [0, 2, 6],
+        [0, 3, 6], [0, 4, 6], [0, 5, 6]
+    ]
+    piece['tubes'] = [
+        [0, 0, 0], [0, 1, 0], [0, 2, 0],
+        [0, 3, 0], [0, 4, 0], [0, 5, 0]
+    ]
+    piece['image-path'] = "./lego_pictures/" + str(3307)
+    piece_list.append(piece)
+    return piece_list
 
 
 def create_dict_for_json(id, length, width, height, flag=True):
